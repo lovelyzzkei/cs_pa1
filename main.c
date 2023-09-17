3d05564c08b7d2372e0bb6aa00c80cb654180ae5
Jinkyu Jeong <jinkyu@yonsei.ac.kr>
2023-09-17 13:21:36 +0000

    output format modified

--- main.c
+++ main.c
@@ -21,20 +21,20 @@ int main(void)
     union {float f; unsigned u;} result4;
 
     scanf("%d\n", &input1);
-    printf("int2bf16(%d) = 0x%hx\n", input1, int2bf16(input1));
+    printf("int2bf16(%d) = 0x%04hx\n", input1, int2bf16(input1));
     scanf("%hx\n", &input2);
-    printf("bf162int(0x%hx) = %d\n", input2, bf162int(input2));
+    printf("bf162int(0x%04hx) = %d\n", input2, bf162int(input2));
     scanf("%f\n", &input3);
-    printf("float2bf16(%f) = 0x%hx\n", input3, float2bf16(input3));
+    printf("float2bf16(%f) = 0x%04hx\n", input3, float2bf16(input3));
     scanf("%hx\n", &input4);
     result4.f = bf162float(input4);
-    printf("bf162float(%hx) = %f (0x%8x)\n", input4, result4.f, result4.u);
+    printf("bf162float(%04hx) = %f (0x%08x)\n", input4, result4.f, result4.u);
     scanf("%hx %hx\n", &input5, &input6);
-    printf("bf16_add(0x%hx, 0x%hx) = 0x%hx\n", input5, input6, bf16_add(input5, input6));
+    printf("bf16_add(0x%04hx, 0x%04hx) = 0x%04hx\n", input5, input6, bf16_add(input5, input6));
     scanf("%hx %hx\n", &input7, &input8);
-    printf("bf16_mul(0x%hx, 0x%hx) = 0x%hx\n", input7, input8, bf16_mul(input7, input8));
+    printf("bf16_mul(0x%04hx, 0x%04hx) = 0x%04hx\n", input7, input8, bf16_mul(input7, input8));
 
-    printf("put_bf16(0x%hx) is ", input1);
+    printf("put_bf16(0x%04hx) is ", input1);
     put_bf16(input1);
     putchar('\n');
 
